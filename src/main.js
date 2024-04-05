43e081fa6e861c83bd39a490e9f583aff0b114b0
Joshua Permito <dwigoric@insanedevs.com>
2024-04-05 23:05:32 +0800

    feat: setup Vuetify

--- src/main.js
+++ src/main.js
@@ -1,14 +1,31 @@
-import './assets/main.css'
-
+// Package imports
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createVuetify } from 'vuetify'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
 
+// Vue imports
 import App from './App.vue'
 import router from './router'
 
+// Vuetify
+import './assets/main.css'
+import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css'
+
+// Build app
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(
+    createVuetify({
+        icons: {
+            defaultSet: 'mdi',
+            aliases,
+            sets: { mdi }
+        }
+    })
+)
 
 app.mount('#app')
