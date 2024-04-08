d368be828d513db2b7d207f3432fa4df69dc4600
Joshua Permito <dwigoric@insanedevs.com>
2024-04-08 18:24:45 +0800

    fix(router): add route for root

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,12 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
-    routes: []
+    routes: [
+        {
+            path: '/',
+            name: 'home'
+        }
+    ]
 })
 
 export default router
